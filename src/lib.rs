0d8f27de194a895e5de2909e1acb66dbca66d7aa
Yeonjae Lee <pusungwi@gmail.com>
2023-08-27 17:48:37 +0900

    Add some skeleton struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,76 @@
+use std::{str::Bytes, result};
+
+struct FileHeader {
+    data_size:usize,
+}
+
+trait BlockHeaderBase {
+    fn signature(&self) -> String;
+    fn size(&self) -> Option<usize>;
+}
+
+struct MidiBlockHeader {
+    signature:String,
+    size:usize, 
+}
+
+impl BlockHeaderBase for MidiBlockHeader {
+    fn signature(&self) -> String {
+        self.signature.clone()
+    }
+
+    fn size(&self) -> Option<usize> {
+        Some(self.size.clone())
+    }
+}
+
+struct WaveBlockHeader {
+    signature:String,
+    size:usize, 
+}
+
+impl BlockHeaderBase for WaveBlockHeader {
+    fn signature(&self) -> String {
+        self.signature.clone()
+    }
+
+    fn size(&self) -> Option<usize> {
+        Some(self.size.clone())
+    }
+}
+
+pub enum MMFParseResult {
+    OK,
+    NOT_FOUND_SAMF_HEADER,
+    UNKNOWN_ERROR,
+}
+
+pub struct MMFFileInfo {
+    result:MMFParseResult,
+    header:FileHeader,
+    midi_blocks:Vec<MidiBlockHeader>,
+    wave_blocks:Vec<WaveBlockHeader>,
+}
+
+impl MMFFileInfo {
+    pub fn new() -> MMFFileInfo {
+        MMFFileInfo {
+            result: MMFParseResult::UNKNOWN_ERROR,
+            header: FileHeader { data_size: 0 },
+            midi_blocks: Vec::new(),
+            wave_blocks: Vec::new(),
+        }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub fn parse(file:Vec<Bytes>) -> MMFFileInfo {
+    let mut file_info:MMFFileInfo = MMFFileInfo::new();
+    file_info.result = MMFParseResult::UNKNOWN_ERROR;
+    file_info
 }
 
 #[cfg(test)]
@@ -9,6 +80,6 @@ mod tests {
     #[test]
     fn it_works() {
         let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(result, 0);
     }
 }
