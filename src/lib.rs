a810cebfb525da8aae21bb95f0971cddbd7cef6b
Yeonjae Lee <pusungwi@gmail.com>
2023-08-30 22:32:27 +0900

    Add more skeleton codes, fixing some comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 use std::{str::Bytes, result};
+use bytestream_rs::bytestream::{ByteStream, ByteStreamError};
 
 struct FileHeader {
     data_size:usize,
+    class:u8,
+    file_type:u8,
+    code_type:u8,
+    status:u8,
+    counts:u8,
+    song_title:String,
+    version:u8,
 }
 
 trait BlockHeaderBase {
@@ -40,40 +48,39 @@ impl BlockHeaderBase for WaveBlockHeader {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum MMFParseResult {
+pub enum MmfParseResult {
     OK,
     NOT_FOUND_SMAF_HEADER,
     UNKNOWN_ERROR,
 }
 
-pub struct MMFFileInfo {
-    result:MMFParseResult,
+pub struct MmfFileInfo {
+    result:MmfParseResult,
     header:FileHeader,
     midi_blocks:Vec<MidiBlockHeader>,
     wave_blocks:Vec<WaveBlockHeader>,
 }
 
-impl MMFFileInfo {
-    pub fn new() -> MMFFileInfo {
-        MMFFileInfo {
-            result: MMFParseResult::UNKNOWN_ERROR,
-            header: FileHeader { data_size: 0 },
+impl MmfFileInfo {
+    pub fn new() -> MmfFileInfo {
+        MmfFileInfo {
+            result: MmfParseResult::UNKNOWN_ERROR,
+            header: FileHeader { data_size: 0, class: todo!(), file_type: todo!(), code_type: todo!(), status: todo!(), counts: todo!(), song_title: todo!(), version: todo!() },
             midi_blocks: Vec::new(),
             wave_blocks: Vec::new(),
         }
     }
 }
 
-pub fn parse(file:Vec<Bytes>) -> MMFFileInfo {
-    let mut file_info:MMFFileInfo = MMFFileInfo::new();
-    
-    //If no data in file bytes vector, Just return not found smaf header
-    if file.len() <= 0 {
-        file_info.result = MMFParseResult::NOT_FOUND_SMAF_HEADER;
+pub fn parse(file:Vec<u8>) -> MmfFileInfo {
+    let mut file_info:MmfFileInfo = MmfFileInfo::new();
+
+    let mut stream = ByteStream::new_from_buffer(file);
+    //If not found data in file bytes vector, Just return not found smaf header
+    if stream.length <= 0 {
+        file_info.result = MmfParseResult::NOT_FOUND_SMAF_HEADER;
     }
 
-    
-    
     file_info
 }
 
@@ -84,6 +91,6 @@ mod tests {
     #[test]
     fn it_works() {
         let info = parse(Vec::new());
-        assert_eq!(info.result, MMFParseResult::OK);
+        assert_eq!(info.result, MmfParseResult::OK);
     }
 }
